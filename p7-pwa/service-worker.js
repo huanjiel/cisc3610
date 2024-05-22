b714f3767c6bdf603c318acffdb8188501b4bd3c
Huanjie Li <54951115+huanjiel@users.noreply.github.com>
2024-05-22 13:28:40 -0400

    pwa test 1

---
+++ p7-pwa/service-worker.js
@@ -0,0 +1,24 @@
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('my-cache').then((cache) => {
+      return cache.addAll([
+        './index.html',
+        './manifest.json',
+        './app.js',
+        './service-worker.js',
+        './icon-192x192.png',
+        './icon-512x512.png',
+        './style.css',
+        './script.js'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
